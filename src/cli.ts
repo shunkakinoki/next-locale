3f4bc1b6358a3c79d5070ff55c0c6fe4d109410a
Shun Kakinoki <shunkakinoki@gmail.com>
2020-10-11 08:17:42 +0900

    feat: 🎸 ini i18n cli

--- src/cli.ts
+++ src/cli.ts
@@ -1,14 +1,39 @@
-const num: number = +process.argv[2];
+import fs from "fs";
+import path from "path";
 
-console.log(fizzbuzz(num));
+const i18nFile = path.resolve(process.cwd(), "i18n.json");
 
-function fizzbuzz(num: number): string {
-  if (num % 15 == 0) {
-    return "FizzBuzz";
-  } else if (num % 3 == 0) {
-    return "Fizz";
-  } else if (num % 5 == 0) {
-    return "Buzz";
-  }
-  return num.toString();
+/* Check for i18n.json */
+if (!fs.existsSync(i18nFile)) {
+  console.warn(`Please put i18n.json at the root.`);
+  process.exit(1);
 }
+
+/* Parse i18n.json */
+var json = JSON.parse(fs.readFileSync(i18nFile, "utf8"));
+
+/* Assign each object */
+const {
+  allLanguages = [],
+  currentPagesDir = "pages_",
+  defaultLanguage = "en",
+  finalPagesDir = "pages",
+  localesPath = "locales",
+  pages = {},
+}: {
+  allLanguages: string[];
+  currentPagesDir: string;
+  defaultLanguage: string;
+  finalPagesDir: string;
+  localesPath: string;
+  pages: {};
+} = json;
+
+console.log(
+  allLanguages,
+  currentPagesDir,
+  defaultLanguage,
+  finalPagesDir,
+  localesPath,
+  pages,
+);
