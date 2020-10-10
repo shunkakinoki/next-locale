05420b07e0d26bd34d85a949dcbe31e5d0b39e82
Shun Kakinoki <shunkakinoki@gmail.com>
2020-10-10 14:41:00 +0900

    feat: 🎸 ini example project

---
+++ examples/next-locale/pages/index.tsx
@@ -0,0 +1,46 @@
+import Link from "next/link";
+import {useRouter} from "next/router";
+
+export default function Index(props) {
+  const router = useRouter();
+
+  return (
+    <>
+      <p id="index">index page</p>
+      <p id="props">{JSON.stringify(props)}</p>
+      <p id="router-locale">{router.locale}</p>
+      <p id="router-locales">{JSON.stringify(router.locales)}</p>
+      <p id="router-query">{JSON.stringify(router.query)}</p>
+      <p id="router-pathname">{router.pathname}</p>
+      <p id="router-as-path">{router.asPath}</p>
+      <Link href="/two">
+        <a id="to-two">to /two</a>
+      </Link>
+      <br />
+      <Link href="/gsp">
+        <a id="to-gsp">to /gsp</a>
+      </Link>
+      <br />
+      <Link href="/gsp/fallback/one">
+        <a id="to-fallback-one">to /gsp/fallback/one</a>
+      </Link>
+      <br />
+      <Link href="/gsp/fallback/two">
+        <a id="to-fallback-two">to /gsp/fallback/two</a>
+      </Link>
+      <br />
+      <Link href="/gsp/no-fallback/one">
+        <a id="to-no-fallback-one">to /gsp/no-fallback/one</a>
+      </Link>
+      <br />
+      <Link href="/gssp">
+        <a id="to-gssp">to /gssp</a>
+      </Link>
+      <br />
+      <Link href="/gssp/one">
+        <a id="to-gssp-slug">to /gssp/one</a>
+      </Link>
+      <br />
+    </>
+  );
+}
