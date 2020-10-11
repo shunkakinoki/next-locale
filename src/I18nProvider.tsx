d4de1b8113e3be8bc975ef5bbcf5ab5425456fc2
Shun Kakinoki <shunkakinoki@gmail.com>
2020-10-12 07:43:29 +0900

    perf: ⚡️ ini i18n Provider

--- src/I18nProvider.tsx
+++ src/I18nProvider.tsx
@@ -1,13 +1,28 @@
 import * as React from "react";
+import I18nContext from "./context";
 
 export interface Props {
   namespaces: {[key: string]: number};
   children: React.ReactNode;
-  internals: {};
 }
 
-export const setInternals = (l: {}) => Object.assign({}, l);
+const NsContext = React.createContext({});
 
 export default function I18nProvider({namespaces, children}: Props) {
-  return <>{children}</>;
+  const ns = React.useContext(NsContext);
+  const allNamespaces = {...ns, ...namespaces};
+
+  function t(key = "") {
+    const k = Array.isArray(key) ? key[0] : key;
+    const [namespace, i18nKey] = k.split(":");
+
+    return `allNamespaces: ${JSON.stringify(
+      allNamespaces,
+    )}, namespace: ${namespace}, i18nKey: ${i18nKey}`;
+  }
+  return (
+    <I18nContext.Provider value={{t}}>
+      <NsContext.Provider value={allNamespaces}>{children}</NsContext.Provider>
+    </I18nContext.Provider>
+  );
 }
