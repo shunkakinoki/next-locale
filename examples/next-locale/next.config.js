63dc4d33e41b07431e05ee3c45b041cfeeb9458a
Shun Kakinoki <shunkakinoki@gmail.com>
2020-10-12 14:15:33 +0900

    feat: 🎸 ini next build

--- examples/next-locale/next.config.js
+++ examples/next-locale/next.config.js
@@ -1,8 +1,10 @@
 module.exports = {
+  target: "experimental-serverless-trace",
   experimental: {
     i18n: {
       locales: ["en", "ja", "zh"],
       defaultLocale: "en",
+      localeDetection: false,
     },
   },
 };
